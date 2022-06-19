9b60190ef7cf1133024a341de459de42ac9da78a
kimyoonseong <dannyk97@naver.com>
2022-06-19 19:23:09 +0900

    플레이어, 카메라
    플레이어 이동, 카메라고정, 미니맵

---
+++ TeamProject-KKJ/Script(Github)/CAM.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CAM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject go_Target;
+    [SerializeField]
+    private float speed;
+    private Vector3 difValue;
+    void Start()
+    {
+        difValue = transform.position - go_Target.transform.position;
+        difValue = new Vector3(Mathf.Abs(difValue.x), Mathf.Abs(difValue.y), (difValue.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.Lerp(this.transform.position, go_Target.transform.position + difValue, speed);
+    }
+}
